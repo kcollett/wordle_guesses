d1d053169f6b3a064d8674a3df818864f0727c4d
kcollett <karen.collett@gmail.com>
2022-08-08 08:49:12 -0500

    Increment version, incorporate Black formatting.

--- src/wordle_guesses/__init__.py
+++ src/wordle_guesses/__init__.py
@@ -4,6 +4,6 @@ wordle_guesses: print out list of candidate Wordle guesses
 
 # NB: Version needs to be manually updated with that in setup.cfg
 # Looked at pbr, versioneer, and bumpversion, but none of them are a good fit
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __authors__ = ["Karen Collett"]
-__license__ = 'MIT'
+__license__ = "MIT"
